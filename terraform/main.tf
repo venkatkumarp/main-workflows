347bf1a8a9b21241ad711fe7d2d32e24272a491a
venkatkumarp <venkat.linux2015@gmail.com>
2024-11-27 10:52:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 # Define the AWS provider
 provider "aws" {
   region = "us-east-1" # Replace with your desired region
@@ -18,17 +17,16 @@ terraform {
     key            = "path/test/terraform.tfstate" # Path to the state file in the bucket
     region         = "us-east-1" # Replace with your bucket's region
     encrypt        = true        # Encrypt state file at rest (recommended)
-    #dynamodb_table = "terraform-lock-table" # Optional: For state locking
+    # Uncomment the following line if using state locking with DynamoDB
+    # dynamodb_table = "terraform-lock-table" 
   }
 }
 
-
-
 ###########################################
-# Data source to fetch the S3 bucket object (zip file)
-data "aws_s3_bucket_object" "lambda_zip" {
+# Data source to fetch the S3 object (zip file)
+data "aws_s3_object" "lambda_zip" {
   bucket = "tftest8"
-  key = "test/lambda-function.zip"
+  key    = "test/lambda-function.zip"
 }
 
 # Lambda function resource using the S3 zip file
@@ -36,14 +34,14 @@ resource "aws_lambda_function" "example_lambda" {
   function_name = "example-lambda-function"
   
   # Use the S3 object as the source
-  s3_bucket = data.aws_s3_bucket_object.lambda_function.bucket
-  s3_key = data.aws_s3_bucket_object.lambda-function.key
+  s3_bucket = data.aws_s3_object.lambda_zip.bucket
+  s3_key    = data.aws_s3_object.lambda_zip.key
   
-  # Alternatively, you can use source_code_hash for change detection
-  source_code_hash = data.aws_s3_bucket_object.lambda_function.etag
+  # Change detection
+  source_code_hash = data.aws_s3_object.lambda_zip.etag
   
   handler = "index.handler" # Replace with your handler
-  runtime = "nodejs18.x" # Replace with your runtime
+  runtime = "nodejs18.x"    # Replace with your runtime
   
   # IAM role for the Lambda function
   role = aws_iam_role.lambda_role.arn
@@ -57,8 +55,8 @@ resource "aws_iam_role" "lambda_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -67,8 +65,8 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# Optional: IAM policy to allow basic Lambda execution
+# IAM policy to allow basic Lambda execution
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.lambda_role.name
 }
