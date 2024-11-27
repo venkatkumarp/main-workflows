0d35f8269afa1ca198e772027668e48508247fad
venkatkumarp <venkat.linux2015@gmail.com>
2024-11-27 10:36:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,12 @@ provider "aws" {
   region = "us-east-1" # Replace with your desired region
 }
 
+# Specify Terraform settings and backend
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.0" # Ensure compatibility with AWS resources
     }
   }
   
